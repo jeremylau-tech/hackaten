195f882aa1a82a5bdabf84f7ff9a84cbf5afdb7b
KeisanArtin <keisanartin1@gmail.com>
2023-12-16 21:30:19 +0800

    commit

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import List from './app/screens/List';
 import Details from './app/screens/Details';
+import ImageUpload from './app/screens/ImageUpload';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,11 +11,10 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name= "My ToDo's" component={List} />
-        <Stack.Screen name= "Details" component={Details} />
+        <Stack.Screen name="Image Upload" component={ImageUpload} />
+        <Stack.Screen name="My ToDo's" component={List} />
+        <Stack.Screen name="Details" component={Details} />
       </Stack.Navigator>
-
     </NavigationContainer>
-
   );
-};
+}
---
+++ app/screens/ImageUpload.tsx
@@ -0,0 +1,111 @@
+import { addDoc, collection, doc, getDocs, deleteDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { Button, View, StyleSheet, ScrollView, Image } from "react-native";
+import * as FileSystem from 'expo-file-system';
+import * as ImagePicker from 'expo-image-picker';
+import { FIREBASE_DB } from "../../firebaseConfig";
+
+const imgDir = FileSystem.documentDirectory + 'images/';
+const IMAGE_ID = 'pic1';
+
+async function ensureDirExists() {
+    const dirInfo = await FileSystem.getInfoAsync(imgDir);
+    if (!dirInfo.exists) {
+        await FileSystem.makeDirectoryAsync(imgDir, { intermediates: true });
+    }
+}
+
+const ImageUpload = () => {
+    const [images, setImages] = useState<string[]>([]);
+
+    useEffect(() => {
+        loadImages();
+    }, []);
+
+    async function loadImages() {
+        const imageCollection = collection(FIREBASE_DB, 'images');
+        const imageSnapshot = await getDocs(imageCollection);
+
+        if (!imageSnapshot.empty) {
+            // Retrieve the image URI from the database
+            const imageUri = imageSnapshot.docs[0].data().uri;
+            setImages([imageUri]);
+        }
+    }
+
+    async function selectImage(useLibrary: boolean) {
+        let result;
+
+        const options: ImagePicker.ImagePickerOptions = {
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 0.75,
+        };
+
+        if (useLibrary) {
+            result = await ImagePicker.launchImageLibraryAsync(options);
+        } else {
+            await ImagePicker.requestCameraPermissionsAsync();
+            result = await ImagePicker.launchCameraAsync(options);
+        }
+
+        if (!result.canceled) {
+            console.log(result.assets[0].uri);
+            await saveImage(result.assets[0].uri);
+        }
+    }
+
+    const saveImage = async (uri: string) => {
+        await ensureDirExists();
+        const filenaname = new Date().getTime() + '.jpg';
+        const dest = imgDir + filenaname;
+        await FileSystem.copyAsync({ from: uri, to: dest });
+
+        const imageCollection = collection(FIREBASE_DB, 'images');
+        
+        // Check if there's an existing image document
+        const existingImageSnapshot = await getDocs(imageCollection);
+        if (!existingImageSnapshot.empty) {
+            // Delete the old image document
+            const oldImageDocId = existingImageSnapshot.docs[0].id;
+            await deleteDoc(doc(imageCollection, oldImageDocId));
+        }
+
+        // Save the new image URI to Firebase
+
+        await addDoc(imageCollection, { uri: dest });
+
+        // Update the state with the new image
+        setImages([dest]);
+    };
+
+    return (
+        <View>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', marginVertical: 20 }}>
+                <Button title="Photo Library" onPress={() => selectImage(true)} />
+                <Button title="Capture Image" onPress={() => selectImage(false)} />
+            </View>
+            <ScrollView>
+                {images.map((img) => (
+                    <Image key={img} source={{ uri: img }} style={{ width: 300, height: 300, alignSelf: "center" }} />
+                ))}
+            </ScrollView>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        marginVertical: 20,
+    },
+    button: {
+        backgroundColor: 'lightblue',
+        padding: 10,
+        borderRadius: 5,
+    },
+});
+
+export default ImageUpload;
